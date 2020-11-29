22f98a2d46ee726c8e0fa4ea402330c5f77c2ea7
楊鵬耀 <b27294301@gmail.com>
2020-11-30 01:27:35 +0800

    fix: GameDetail Detail 樣式微調

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -136,8 +136,7 @@ const CardShadow = styled(motion.div)`
 `;
 
 const Detail = styled(motion.div)`
-  width: 90%;
-  max-width: 1024px;
+  width: 80%;
   border-radius: 1rem;
   padding: 2rem 5rem;
   background: #fff;
